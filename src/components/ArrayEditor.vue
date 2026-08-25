b45376e045dc6821acb2247146f7f4be0784c47e
陨落基围虾 <3161880837@qq.com>
2026-08-25 23:02:52 +0800

    refactor(ArrayEditor): 改用 defineModel 简化数组双向绑定

--- src/components/ArrayEditor.vue
+++ src/components/ArrayEditor.vue
@@ -1,56 +1,42 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
 
+const model = defineModel<(string | number)[]>({ required: true });
 const props = defineProps<{
-    modelValue: (string | number)[];
     type?: "string" | "number";
 }>();
 
-const emit = defineEmits<{
-    (e: "update:modelValue", value: (string | number)[]): void;
-}>();
+const items = ref<string[]>(model.value.map(String));
 
-const items = ref<string[]>(props.modelValue.map(String));
+watch(model, (v) => {
+    items.value = v.map(String);
+});
 
-watch(
-    () => props.modelValue,
-    (v) => {
-        items.value = v.map(String);
-    },
-);
-
-function emitUpdate() {
+function commit() {
     const cleaned = items.value.map((x) => x.trim()).filter(Boolean);
-    emit(
-        "update:modelValue",
-        props.type === "number" ? cleaned.map(Number) : cleaned,
-    );
+    model.value = props.type === "number" ? cleaned.map(Number) : cleaned;
 }
 
 function add() {
-    items.value.push("");
-    emitUpdate();
+    items.value.push("0");
+    commit();
 }
 
 function changeAt(i: number, v: string) {
     items.value[i] = v;
-    emitUpdate();
+    commit();
 }
 
 function removeAt(i: number) {
     items.value.splice(i, 1);
-    emitUpdate();
+    commit();
 }
 </script>
 <template>
     <div>
         <template v-for="(item, i) in items" :key="i">
-            <input
-                :type="type === 'number' ? 'number' : 'text'"
-                :value="item"
-                @input="changeAt(i, ($event.target as HTMLInputElement).value)"
-                size="16"
-            />
+            <input :type="type === 'number' ? 'number' : 'text'" :value="item"
+                @input="changeAt(i, ($event.target as HTMLInputElement).value)" size="16" />
             <button @click="removeAt(i)">删除</button>
             <br />
         </template>
